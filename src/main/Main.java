2477937b9f6030b677a486fd635b790a08633b9a
Ahmad Khanafer <ahmad.khanafer.190@gmail.com>
2024-11-24 23:06:59 -0500

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Main frame = new Main();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public Main() {
+        setTitle("FlappyBird");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 702, 506);
+        setSize(650, 400);
+
+    }
+}
